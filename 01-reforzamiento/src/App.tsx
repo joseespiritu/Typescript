d133956e6b8f2eeae918792340b4f62e9e844bc8
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-04-20 21:32:28 -0500

    Objetos Literales

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,11 +1,13 @@
-import { TiposBasicos } from "./typescript/TiposBasicos"
+//import { TiposBasicos } from "./typescript/TiposBasicos"
+import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
 
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Introduccion a TS-React</h1>
       <hr/>
-      <TiposBasicos/>
+      {/* <TiposBasicos/> */}
+      <ObjetosLiterales/>
     </div>
   );
 }
