bb43111fc136cf0e7f39e846bd5c830e46b70c6e
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-04-20 21:20:45 -0500

    Tipos basicos

--- 01-reforzamiento/src/typescript/TiposBasicos.tsx
+++ 01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,7 +1,17 @@
 export const TiposBasicos = () => {
+
+    const nombre: string = 'Jose';
+    const edad: number = 24;
+    const estaActivo: boolean = true; // Los boolean no tienen representacion en html
+
+    const poderes:string[] = ['Velocidad', 'Volar', 'Respirar en el agua'];
+
     return(
         <>
             <h3>Tipos básicos</h3>
+            { nombre }, { edad }. { (estaActivo) ? 'activo' : 'no activo' }
+            <hr/>
+            { poderes.join(', ') }
         </>
     );
 }
