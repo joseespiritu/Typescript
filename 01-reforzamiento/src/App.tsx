890f68032eba3c223a9416aa9d3091339cfc9740
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-04-23 23:46:42 -0500

    customHook, useCounter

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,8 +1,9 @@
 //import { TiposBasicos } from "./typescript/TiposBasicos"
 //import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
 //import { Funciones } from "./typescript/Funciones"
+//import { Contador } from "./components/Contador"
 
-import { Contador } from "./components/Contador"
+import { ContadorConHook } from "./components/ContadorConHook"
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       {/* <TiposBasicos/> */}
       {/* <ObjetosLiterales/> */}
       {/* <Funciones /> */}
-      <Contador/>
+      {/* <Contador/> */}
+      <ContadorConHook />
     </div>
   );
 }
