478e9ea35073be04f51327fc5f5cf7f64d8a1b44
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-12 21:08:27 -0500

    Peticiones HTTP con Axios, configuracion inicial

---
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { reqResApi } from '../api/reqRes';
+
+export const Usuarios = () => {
+
+    useEffect(() => {
+        // llamado al API
+        reqResApi.get('/users')
+            .then( resp => {
+                console.log(resp.data.data);
+            })  
+            .catch(console.log);
+    }, [])
+
+    return (
+        <div>
+            <h3>Usuarios: </h3>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+            </table>
+            <tbody>
+
+            </tbody>
+        </div>
+    )
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Respositorio dedicado a aprender typescript
 ### Implementando proyecto template
 * [link](https://create-react-app.dev/docs/getting-started/) _npx create-react-app my-app --template typescript_
 * Bootstrap: [Link](https://getbootstrap.com/)
+* API para probar [Req Res](https://reqres.in/)
+* Axios para peticiones [npm axios](https://www.npmjs.com/package/axios) _npm i axios_ ó _yarn add axios_
