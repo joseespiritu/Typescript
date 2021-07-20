8cdb3a940f4f3da3ac830f5071bcf732bba9ce49
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-20 01:38:29 -0500

    customHook

--- 01-reforzamiento/src/components/Usuarios.tsx
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -1,50 +1,25 @@
-import { useEffect, useState, useRef } from 'react';
-import { reqResApi } from '../api/reqRes';
-import { ReqResListado, Usuario } from '../interfaces/reqRes';
+import { useUsuarios } from "../hooks/useUsuarios";
+import { Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
 
-    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+    const { usuarios, cargarUsuarios } = useUsuarios();
 
-    const paginaRef = useRef(1);
-
-    useEffect(() => {
-        // llamado al API
-        cargarUsuarios();
-    }, []);
-
-    const cargarUsuarios = async () => {
-
-        const resp = await reqResApi.get<ReqResListado>('/users', {
-            params: {
-                page: paginaRef.current
-            }
-        })
-
-        if( resp.data.data.length > 0 ){
-            setUsuarios( resp.data.data );
-            paginaRef.current ++;
-        } else {
-            alert('No hay más registros');
-        }
-
-    }
-
-    const renderItem = ( { id, first_name, last_name, email, avatar}: Usuario ) => {
+    const renderItem = ({ id, first_name, last_name, email, avatar }: Usuario) => {
         return (
-            <tr key={ id.toString() }>
+            <tr key={id.toString()}>
                 <td>
                     <img
-                        src={ avatar } 
-                        alt={ first_name }
+                        src={avatar}
+                        alt={first_name}
                         style={{
                             width: 35,
                             borderRadius: 100
                         }}
                     />
                 </td>
-                <td> { first_name } { last_name } </td>
-                <td> { email } </td>
+                <td> {first_name} {last_name} </td>
+                <td> {email} </td>
             </tr>
         )
     }
@@ -66,6 +41,8 @@ export const Usuarios = () => {
                     }
                 </tbody>
             </table>
+
+            &nbsp;
 
             <button
                 className="btn btn-primary"
---
+++ 01-reforzamiento/src/hooks/useUsuarios.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState, useRef } from 'react';
+import { reqResApi } from '../api/reqRes';
+import { ReqResListado, Usuario } from '../interfaces/reqRes';
+
+export const useUsuarios = () => {
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    const paginaRef = useRef(1);
+
+    useEffect(() => {
+        // llamado al API
+        cargarUsuarios();
+    }, []);
+
+    const cargarUsuarios = async () => {
+
+        const resp = await reqResApi.get<ReqResListado>('/users', {
+            params: {
+                page: paginaRef.current
+            }
+        })
+
+        if (resp.data.data.length > 0) {
+            setUsuarios(resp.data.data);
+            paginaRef.current++;
+        } else {
+            alert('No hay más registros');
+        }
+
+    }
+
+    return {
+        usuarios,
+        cargarUsuarios
+    }
+
+}
