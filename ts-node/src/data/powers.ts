b105be4cd2e9019f15b73de1da7f231c6e710604
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-02-15 18:54:13 -0600

    Modulos y webpack

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,14 @@
+
+export interface Power {
+    id: number;
+    desc: string;
+}
+
+const powers: Power[] = [
+    {
+        id: 1,
+        desc: 'Money'
+    }
+];
+
+export default powers;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,13 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers, {Power} from "./data/powers";
 
+// console.log('Hola Mundo!!!!');
+// const Hero = 123;
 
-console.log('Hola Mundo!');
+const ironman = new Hero('Iron man', 1, 35);
 
-
-
-
-
-
+console.log(ironman);
+console.log(ironman.power);
 
